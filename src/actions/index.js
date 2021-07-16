d5d00f6b7c0bc91f8d6d6c05ec6661899ef3df59
Alejandro Vasquez <alejandrovasquez@Alejandros-iMac.local>
2021-07-16 13:18:40 -0700

    Finished MVP

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,30 @@
 import axios from 'axios';
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAIL = "FETCH_FAIL";
+export const ADD_SMURF = "ADD_SMURF";
+export const SET_ERROR = 'SET_ERROR';
+
+export const fetchSmurfs = () => dispatch => {
+    dispatch({ type: FETCH_START });
+
+    axios.get('http://localhost:3333/smurfs')
+    .then((res) => {
+        dispatch({ type: FETCH_SUCCESS, payload: res.data });
+    })
+    .catch((err) => {
+        dispatch({ type: FETCH_FAIL, payload: `${err.response.message}, code: ${err.response.code}` });
+        console.log(err);
+    });
+}
+
+export const addSmurf = (smurf) => {
+    return({ type: ADD_SMURF, payload: smurf })
+}
+
+export const setError = (error) => {
+    return({ type: SET_ERROR, payload: error })
+}
 
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import "./index.css";
 import App from "./App";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import reducer from './reducers/index'
 
 const { worker } = require('./mocks/browser');
 worker.start();
 
 const rootElement = document.getElementById("root");
 
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>, 
     rootElement
 );
 
